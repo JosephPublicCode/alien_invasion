a5458299b27589bdb1b7899169b1b09c45fe5d3e
Joseph1550 <joedaws1@gmail.com>
2024-08-18 19:07:43 +0100

    update 18082024

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,10 +29,6 @@ class AlienInvasion:
         self.stats = GameStats(self)
         
 
-        # self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-        # self.settings.screen_width = self.screen.get_rect().width
-        # self.settings.screen_height = self.screen.get_rect().height
-
         self.screen = pygame.display.set_mode((self.settings.screen_width,\
                                                self.settings.screen_height))
         
